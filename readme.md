c63c5346da245df0feed7f06ecb5213b33e4308b
Weslleyw10 <weslleylopes.dev@gmail.com>
2021-11-02 11:19:07 -0300

    app finished

---
+++ readme.md
@@ -0,0 +1,108 @@
+<h1 align="center"><strong>TODO API</strong></h1>
+
+<h4 align="center"> 
+	Todo api 🚀 Finished...
+</h4>
+
+<br />
+
+## Resources
+
+- [x] Create user
+- [x] Middleware user
+- [x] List users
+- [x] Create todo
+- [x] List todos
+- [x] Update todo
+- [x] Done todo
+- [x] Delete todo
+
+<br />
+
+## Requirements
+
+Before starting, you will need to have the following tools installed on your machine:
+[Git](https://git-scm.com), [Node.js](https://nodejs.org/en/). 
+Use an editor of your choice.
+
+<br />
+
+## 🎲 Running
+
+```bash
+# Clone this repository
+$ git clone <https://github.com/tgmarinho/nlw1>
+
+# Access the directory this project from the terminal/cmd
+$ cd ignite-template-conceitos-do-nodejs
+
+# Install dependencies
+$ yarn install
+
+# Run
+$ yarn install
+
+# The server will start on port:8000 - access <http://localhost:8000>
+```
+
+<br />
+
+# Endpoints
+
+## User
+
+``` bash
+    # create user 
+    curl --request POST \
+  --url http://127.0.0.1:8000/users \
+  --header 'Content-Type: application/json' \
+  --data '{"name": "Andre", "username": "andre.gio"}'
+```
+``` bash
+    # list users
+    curl --request GET \
+  --url http://127.0.0.1:8000/users
+```
+
+## Todos
+
+``` bash
+    # Create todos
+    curl --request POST \
+  --url http://127.0.0.1:8000/todos \
+  --header 'Content-Type: application/json' \
+  --header 'username: andre.gio' \
+  --data '{ 	
+	"title": "Nome da tarefa",
+	"deadline": "2021-11-02"
+    }'
+```
+``` bash
+    # List todos
+    curl --request GET \
+  --url http://127.0.0.1:8000/todos \
+  --header 'username: {username}'
+```
+``` bash
+    # Update todo
+    curl --request PUT \
+  --url http://127.0.0.1:8000/todos/{todoId} \
+  --header 'Content-Type: application/json' \
+  --header 'username: {username}' \
+  --data '{ 	
+	"title": "Nome da tarefa",
+	"deadline": "2021-11-03"
+    }'
+```
+``` bash
+    # Complete todo
+    curl --request PATCH \
+  --url http://127.0.0.1:8000/todos/{todoId}/done \
+  --header 'username: {username}'
+```
+``` bash
+    # Delete todo
+    curl --request DELETE \
+  --url http://127.0.0.1:8000/todos/{todoId} \
+  --header 'username: {username}'
+```
--- src/index.js
+++ src/index.js
@@ -1,41 +1,126 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+
+  const user = users.find(user => user.username === username)
+
+  if(!user) {
+    return response.status(400).json({error: true, message: "Username not exists."})
+  }
+
+  request.username = username
+  return next()
+
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body
+
+  const verifyIfAlreadyExistsUser = users.some(user => user.username === username);
+
+  if (verifyIfAlreadyExistsUser) {
+    return response.status(400).json('Username already exists.')
+  }
+
+  const user = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  }
+
+  users.push(user);
+  response.json(user)
+
 });
 
+app.get('/users', (request, response) => {
+  response.json(users)
+})
+
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { username } = request
+  const user = users.find(user => user.username === username)
+  response.json(user)
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { username } = request
+  const { title, deadline } = request.body
+  const user = users.find(user => user.username === username)
+
+  const todo = {
+    id: uuidv4(),
+    title,
+    deadline: new Date(deadline),
+    done: false,    
+    created_at: new Date()
+  }
+
+  user.todos.push(todo)
+  response.json(todo)
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { username } = request
+  const { id } = request.params
+  const { title, deadline } = request.body
+
+  const user = users.find(user => user.username === username)
+  const todo = user.todos.find(todo => todo.id === id)
+
+  if(!todo) {
+    return response.status(400).json({ error: true, message: "Todo not found."})
+  }
+
+  todo.title = title,
+  todo.deadline = new Date(deadline)
+  response.json(todo)
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { username } = request
+  const { id } = request.params
+
+  const user = users.find(user => user.username === username)
+  const todo = user.todos.find(todo => todo.id === id)
+
+  if(!todo) {
+    return response.status(400).json({ error: true, message: "Todo not found."})
+  }
+
+  todo.done = true
+
+  response.json(user)
+
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+    const { username } = request
+    const { id } = request.params
+
+    const user = users.find(user => user.username === username)
+    const todo = user.todos.find(todo => todo.id === id)
+
+    if(!todo) {
+      return response.status(400).json({ error: true, message: "Todo not found."})  
+    }
+
+    user.todos.splice(todo.id, 1)
+
+    response.json("Todo deleted successfully.")
+
 });
 
 module.exports = app;
